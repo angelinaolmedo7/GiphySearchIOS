3b5f219d154a8101ff11998dd1761f7130ee53c7
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-04-13 04:17:37 -0700

    Initial Commit

---
+++ GiphySearchIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphySearchIOS
+//
+//  Created by Angelina Olmedo on 4/13/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
