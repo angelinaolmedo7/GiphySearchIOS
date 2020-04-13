26a9c89f2d12a32eca82da99142daac76cf3d279
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-04-13 04:30:13 -0700

    Page 3

---
+++ GiphySearchIOS/GifCell.swift
@@ -0,0 +1,22 @@
+//
+//  GifCell.swift
+//  GiphySearchIOS
+//
+//  Created by Angelina Olmedo on 4/13/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class GifCell: UITableViewCell {
+override func layoutSubviews() {
+    super.layoutSubviews()
+    // Add views
+}
+override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+}
+required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+}
+}
--- GiphySearchIOS/ViewController.swift
+++ GiphySearchIOS/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+    }
+    // Setup tableview delegates.
+    func setup() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Search bar
+        searchBar.searchTextField.delegate = self
+        searchBar.searchTextField.placeholder = "Whats your favorite gif?"
+        searchBar.returnKeyType = .search
     }
 
 
 }
 
+// MARK: - Tableview functions
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "gifCell") as! GifCell
+        return cell
+    }
+}
+
+// MARK: - Search bar functions
+extension ViewController: UISearchTextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if textField.text != nil {
+            print(textField.text!)
+        }
+        return true
+    }
+}
